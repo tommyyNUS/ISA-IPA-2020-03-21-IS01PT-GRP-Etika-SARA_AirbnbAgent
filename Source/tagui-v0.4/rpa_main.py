f6cf0f85478c07184ebd85fbbbeb218da0fb7152
Tommy Yong <tommyyong88@hotmail.com>
2020-03-29 14:17:46 +0800

    Committing update of rpa_main.py

--- Source/tagui-v0.4/rpa_main.py
+++ Source/tagui-v0.4/rpa_main.py
@@ -3,26 +3,38 @@ import rpa as r
 import airbnb_rpa_functions as airbnb
 import time
 
-start = time.time()
+def process(country, numOfAdults, numOfInfants, numOfChildren, fromDate, toDate):
+    start = time.time()
 
-airbnb.initialize()
-r.wait()
-airbnb.select_stay()
-r.wait()
-airbnb.enter_country_city("aomori")
-airbnb.close_cookie_popup()
-r.wait()
-airbnb.enter_dates("25 December 2020","30 December 2020")
-r.wait()
-airbnb.enter_personnel(1,2,3)
-#airbnb.recreate_temp()
-airbnb.snap_map()
-data = airbnb.extract_stay_info_as_data()
-print(data)
-r.close()
+    airbnb.initialize()
+    r.wait()
+    airbnb.select_stay()
+    r.wait()
+    airbnb.enter_country_city(country)
+    airbnb.close_cookie_popup()
+    r.wait()
+    airbnb.enter_dates(fromDate,toDate)
+    r.wait()
+    #adult, infant, child
+    #airbnb.enter_personnel(1,2,3)
+    airbnb.enter_personnel(numOfAdults,numOfInfants,numOfChildren)
 
-end = time.time()
+    #airbnb.recreate_temp()
+    airbnb.snap_map()
+    data = airbnb.extract_stay_info_as_data()
+    print(data)
+    r.close()
 
-elapsed = end - start
-print(f'Time Elapsed: {elapsed} seconds')
+    end = time.time()
 
+    elapsed = end - start
+    print(f'Time Elapsed: {elapsed} seconds')  
+
+    response =  """
+            Country : {0}
+            Num: {1}
+            From: {2}
+            To: {3}
+        """.format(country, numOfPeople, fromDate, toDate)
+    
+    return response
