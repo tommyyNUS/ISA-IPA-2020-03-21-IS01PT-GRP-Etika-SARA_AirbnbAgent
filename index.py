005872e98fa5cbf0d8a88b3f114f8786e651caa5
necrodawn <necrodawn>
2020-04-01 02:40:58 +0800

    Updated tagui script to v1.0 - Changed the script to account for major UI change in airbnb

--- index.py
+++ index.py
@@ -7,7 +7,7 @@ import requests
 import json
 import pusher
 import sys
-sys.path.insert(0,'./Source/tagui-v0.5/')
+sys.path.insert(0,'./Source/tagui-v1.0/')
 import rpa_main as airbnb
 
 app = Flask(__name__)
