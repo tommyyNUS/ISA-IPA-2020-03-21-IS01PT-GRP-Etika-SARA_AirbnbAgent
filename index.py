3b976003d59bada0421b6a947228224b1dffc4e6
Tommy <tommyyyong88@hotmail.com>
2020-04-03 21:34:47 +0800

    index file changes

--- index.py
+++ index.py
@@ -9,8 +9,14 @@ import pusher
 import sys
 sys.path.insert(0,'./Source/tagui-v1.0/')
 import rpa_main as airbnb
+from pandas.io.json import json_normalize
+import pandas as pd
+import folium
+from geopy.geocoders import Nominatim 
 
 app = Flask(__name__)
+CLIENT_ID = 'NFTR233O0S1IITFK10EHZURRRQOB4UAPI1RYXSKE4HAY4UNU'
+CLIENT_SECRET = 'ECNBJE3PPFMAMT2US14SIWOKNA15FSK03O321JMNZYWFT4PY'
 
 @app.route('/get_recommendations', methods=['POST'])
 def index():
@@ -30,9 +36,9 @@ def index():
 
     fromDate = formatDate(fromDate)
     toDate = formatDate(toDate)
-        
-    response = airbnb.process(country, int(numOfAdults), int(numOfInfants), int(numOfChildren), fromDate, toDate)
-
+    get_venues('test')
+    #response = airbnb.process(country, int(numOfAdults), int(numOfInfants), int(numOfChildren), fromDate, toDate)
+    response="HIHIHI"
     reply = {
             "fulfillmentText": response,
         }
@@ -46,6 +52,36 @@ def formatDate(date):
     formattedDate = formattedDate.strftime('X%d').replace('X0', '').replace('X','') +" "+ formattedDate.strftime('%B') +" "+ formattedDate.strftime('%Y')
 
     return formattedDate
+
+def get_venues(listings):
+    url = 'https://api.foursquare.com/v2/venues/explore'
+
+    params = dict(
+    client_id=CLIENT_ID,
+    client_secret=CLIENT_SECRET,
+    v='20200401',
+    ll='1.323681 ,103.947566',
+    limit=10,
+    radius = 600
+    )
+    resp = requests.get(url=url, params=params).json()
+    
+    venues = resp['response']['groups'][0]['items']
+    #nearby_venues = json_normalize(venues)
+    #columns = ['venue.name', 'venue.categories.[0].name', 'venue.location.lat', 'venue.location.lng']
+    nearby_venues = pd.DataFrame(columns=['Name', 'Category', 'Latitude', 'Longitude'])
+    for venue in venues:
+        df = pd.DataFrame([(venue['venue']['name'], 
+                            venue['venue']['categories'][0]['name'],
+                            venue['venue']['location']['lat'], 
+                            venue['venue']['location']['lng'])], columns=['Name', 'Category', 'Latitude', 'Longitude'])
+        nearby_venues = nearby_venues.append(df, ignore_index=True)
+    print(nearby_venues.head())
+    #map = folium.Map([51., 12.], zoom_start=6,control_scale=True)
+    #folium.GeoJson(data).add_to(map)
+    #map.save('map.html')
+    
+    return ""
 
 # run Flask app
 if __name__ == "__main__":
